9fc1cacafabc629cfc82bb5941079746a44cfc0e
Dzmitry <amigo1044@yandex.ru>
2025-02-19 21:55:30 +0300

    f

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -1,10 +1,27 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class DeveloperProjectFinder {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
+        List<String> list = new ArrayList<>();
+        for (var key : projects.keySet()) {
+            for (var dev : projects.get(key)) {
+                if (dev.equals(developer)) {
+                    list.add(key);
+                    break;
+                }
+            }
+        }
+        list.sort((o1, o2) -> {
+            if (o1.length() > o2.length()) {
+                return -1;
+            } else if (o1.length() < o2.length()) {
+                return 1;
+            } else {
+                return o1.compareTo(o2) * -1;
+            }
+        });
+        return list;
     }
 }
--- src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
+++ src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
@@ -1,9 +1,24 @@
 package com.epam.mjc.collections.combined;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class MapFromKeysCreator {
     public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
+        Map<Integer, Set<String>> map = new HashMap<>();
+        for (String word : sourceMap.keySet()) {
+            Integer value = sourceMap.get(word);
+            if (map.containsKey(word.length())) {
+                var set = map.get(word.length());
+                set.add(word);
+            } else {
+                Set<String> newSet = new HashSet<>();
+                newSet.add(word);
+                map.put(word.length(), newSet);
+            }
+        }
+        return map;
     }
 }
